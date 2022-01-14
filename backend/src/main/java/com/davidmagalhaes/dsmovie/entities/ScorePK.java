a6debf1535c64d940c8b0712327280154ffaa088
david <deivi_gf144@hotmail.com>
2022-01-14 09:40:32 -0400

    First request

--- backend/src/main/java/com/davidmagalhaes/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/davidmagalhaes/dsmovie/entities/ScorePK.java
@@ -8,7 +8,7 @@ import javax.persistence.ManyToOne;
 
 @Embeddable
 public class ScorePK implements Serializable{
-	private static final Long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	@ManyToOne
 	@JoinColumn(name = "movie_id")
